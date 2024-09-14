af210a8b278917bfa00867feebc6c779479f7f2f
Noah0401 <165160435+Noah0401@users.noreply.github.com>
2024-09-14 16:55:40 +0800

    Update zeroshot_llm.py

--- LLMPredictor/zeroshot_llm.py
+++ LLMPredictor/zeroshot_llm.py
@@ -3,19 +3,94 @@ import os
 import argparse
 import torch
 import prettytable as pt
+import csv
+import pandas as pd
+from sklearn.metrics import accuracy_score, f1_score
 
-def cora_response(model_name, index):
+CORA_Q = """Question: Which of the following 
+        sub-categories of AI does this paper belong to? Here are the 7 categories: 
+        Rule_Learning, Neural_Networks, Case_Based, 
+        Genetic_Algorithms, Theory, Reinforcement_Learning, Probabilistic_Methods.
+        Reply only one category that you think this paper might belong to. 
+        Only reply the category name without any other words.
+        Answer:"""
+
+PUBMED_Q = """Question: Which of the following 
+        topic does this scientific publication talk about? Here are the 3 categories: 
+        Experimental, Diabetes Mellitus Type 1, Diabetes Mellitus Type 2.
+        Experimental category usually refers to Experimentally induced diabetes, 
+        Diabetes Mellitus Type 1 usually means the content of the paper is about Diabetes Mellitus Type 1,
+        Diabetes Mellitus Type 2 usually means the content of the paper is about Diabetes Mellitus Type 2.
+        Reply only one category that you think this paper might belong to. Only reply the category name without any other words.
+        Answer:
+    """
+
+CITESEER_Q = """Question: Which of the following 
+        theme does this paper belong to? Here are the 6 categories: 
+        Agents, ML (Machine Learning), IR (Information Retrieval), DB (Databases), 
+        HCI (Human-Computer Interaction), AI (Artificial Intelligence).
+        Reply only one category that you think this paper might belong to. 
+        Only reply the category full name I give you without any other words.
+        Answer:
+    """
+
+WIKICS_Q = """Question: Which of the following 
+        branch of Computer science does this Wikipedia-based dataset belong to? Here are the 10 categories: 
+        Computational Linguistics, Databases, Operating Systems, Computer Architecture, Computer Security, 
+        Internet Protocols, Computer File Systems, 
+        Distributed Computing Architecture, Web Technology, Programming Language Topics.
+        Reply only one category that you think this paper might belong to. Only reply the category full name without any other words.
+        Answer:
+    """
+
+ARXIV_Q = """Question: Which of the following 
+        arXiv CS sub-categories does this dataset belong to? Here are the 40 categories: 
+        'arxiv cs na', 'arxiv cs mm', 'arxiv cs lo', 'arxiv cs cy', 'arxiv cs cr', 
+        'arxiv cs dc', 'arxiv cs hc', 'arxiv cs ce', 'arxiv cs ni', 'arxiv cs cc',
+        'arxiv cs ai', 'arxiv cs ma', 'arxiv cs gl', 'arxiv cs ne', 'arxiv cs sc', 
+        'arxiv cs ar', 'arxiv cs cv', 'arxiv cs gr', 'arxiv cs et', 'arxiv cs sy', 
+        'arxiv cs cg', 'arxiv cs oh', 'arxiv cs pl', 'arxiv cs se', 'arxiv cs lg', 
+        'arxiv cs sd', 'arxiv cs si', 'arxiv cs ro', 'arxiv cs it', 'arxiv cs pf', 
+        'arxiv cs cl', 'arxiv cs ir', 'arxiv cs ms', 'arxiv cs fl', 'arxiv cs ds', 
+        'arxiv cs os', 'arxiv cs gt', 'arxiv cs db', 'arxiv cs dl', 'arxiv cs dm'. 
+        Reply only one category that you think this paper might belong to. 
+        Only reply the category name I given without any other words, please don't use your own words.
+        Answer:
+    """
+
+INSTAGRAM_Q = """Question: Which of the following 
+        categories does this instagram user belong to? Here are the 2 categories: 
+        Normal Users, Commercial Users. 
+        Reply only one category that you think this paper might belong to. 
+        Only reply the category name I give of the category: Normal Users, Commercial Users, without any other words.
+        Answer:
+    """
+
+REDDIT_Q = """Question: Which of the following 
+        categories does this reddit user belong to? Here are the 2 categories: 
+        Normal Users, Popular Users. 
+        Popular Users' posted content are often more attractive.
+        Reply only one category that you think this paper might belong to. 
+        Only reply the category name I give of the category: Normal Users, Popular Users, without any other words.
+        Answer:
+    """
+
+DEFAULT_QUESTION = {
+    "cora_q": CORA_Q,
+    "pubmed_q": PUBMED_Q,
+    "citeseer_q": CITESEER_Q,
+    "wikics_q": WIKICS_Q,
+    "arxiv_q": ARXIV_Q,
+    "instagram_q": INSTAGRAM_Q,
+    "reddit_q": REDDIT_Q,
+}
+
+def cora_response(model_name, index, write_file):
     device = 'cpu'
     data = torch.load(f"../datasets/cora.pt").to(device)
+
     discription = data.raw_texts[index]
-    question = (
-        "Question: Which of the following "
-        "sub-categories of AI does this paper belong to? Here are the 7 categories: "
-        "Rule_Learning, Neural_Networks, Case_Based, "
-        "Genetic_Algorithms, Theory, Reinforcement_Learning, Probabilistic_Methods."
-        "Reply only one category that you think this paper might belong to. Only reply the category name without any other words."
-        "Answer:"
-    )
+    question = DEFAULT_QUESTION["cora_q"]
 
     client = OpenAI(
         api_key=os.getenv("DASHSCOPE_API_KEY"), # 如果您没有配置环境变量，请在此处用您的API Key进行替换
@@ -30,26 +105,20 @@ def cora_response(model_name, index):
             }
         ]
     )
-    print(completion.choices[0].message.content)
-    if completion.choices[0].message.content == data.label_name[data.y[index]]:
-        print ("right")
-    else:
-        print ("false")
 
-def pubmed_response(model_name, index):
+    prediction=completion.choices[0].message.content
+    true_label=data.label_name[data.y[index]]
+
+    with open(write_file, mode='a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([index, prediction, true_label])
+        print([index, prediction, true_label])
+
+def pubmed_response(model_name, index, write_file):
     device = 'cpu'
     data = torch.load(f"../datasets/pubmed.pt").to(device)
     discription = data.raw_texts[index]
-    question = (
-        "Question: Which of the following "
-        "topic does this scientific publication talk about? Here are the 3 categories: "
-        "Experimental, Diabetes Mellitus Type 1, Diabetes Mellitus Type 2."
-        "Experimental category usually refers to Experimentally induced diabetes, "
-        "Diabetes Mellitus Type 1 usually means the content of the paper is about Diabetes Mellitus Type 1,"
-        "Diabetes Mellitus Type 2 usually means the content of the paper is about Diabetes Mellitus Type 2."
-        "Reply only one category that you think this paper might belong to. Only reply the category name without any other words."
-        "Answer:"
-    )
+    question = DEFAULT_QUESTION["pubmed_q"]
 
     client = OpenAI(
         api_key=os.getenv("DASHSCOPE_API_KEY"), # 如果您没有配置环境变量，请在此处用您的API Key进行替换
@@ -64,24 +133,19 @@ def pubmed_response(model_name, index):
             }
         ]
     )
-    print(completion.choices[0].message.content)
-    if completion.choices[0].message.content == data.label_name[data.y[index]]:
-        print ("right")
-    else:
-        print ("false")
+    prediction=completion.choices[0].message.content
+    true_label=data.label_name[data.y[index]]
 
-def citeseer_response(model_name, index):
+    with open(write_file, mode='a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([index, prediction, true_label])
+        print([index, prediction, true_label])
+
+def citeseer_response(model_name, index, write_file):
     device = 'cpu'
     data = torch.load(f"../datasets/citeseer.pt").to(device)
     discription = data.raw_texts[index]
-    question = (
-        "Question: Which of the following "
-        "theme does this paper belong to? Here are the 6 categories: "
-        "Agents, ML (Machine Learning), IR (Information Retrieval), DB (Databases), "
-        "HCI (Human-Computer Interaction), AI (Artificial Intelligence."
-        "Reply only one category that you think this paper might belong to. Only reply the category name without any other words."
-        "Answer:"
-    )
+    question = DEFAULT_QUESTION["citeseer_q"]
 
     client = OpenAI(
         api_key=os.getenv("DASHSCOPE_API_KEY"), # 如果您没有配置环境变量，请在此处用您的API Key进行替换
@@ -96,25 +160,19 @@ def citeseer_response(model_name, index):
             }
         ]
     )
-    print(completion.choices[0].message.content)
-    if completion.choices[0].message.content == data.label_name[data.y[index]]:
-        print ("right")
-    else:
-        print ("false")
+    prediction=completion.choices[0].message.content
+    true_label=data.label_name[data.y[index]]
 
-def wikics_response(model_name, index):
+    with open(write_file, mode='a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([index, prediction, true_label])
+        print([index, prediction, true_label])
+
+def wikics_response(model_name, index, write_file):
     device = 'cpu'
     data = torch.load(f"../datasets/wikics.pt").to(device)
     discription = data.raw_texts[index]
-    question = (
-        "Question: Which of the following "
-        "branch of Computer science does this Wikipedia-based dataset belong to? Here are the 10 categories: "
-        "Computational Linguistics, Databases, Operating Systems, Computer Architecture, Computer Security, "
-        "Internet Protocols, Computer File Systems, "
-        "Distributed Computing Architecture, Web Technology, Programming Language Topics."
-        "Reply only one category that you think this paper might belong to. Only reply the category full name without any other words."
-        "Answer:"
-    )
+    question = DEFAULT_QUESTION["wikics_q"]
 
     client = OpenAI(
         api_key=os.getenv("DASHSCOPE_API_KEY"), # 如果您没有配置环境变量，请在此处用您的API Key进行替换
@@ -129,31 +187,20 @@ def wikics_response(model_name, index):
             }
         ]
     )
-    print(completion.choices[0].message.content)
-    if completion.choices[0].message.content == data.label_name[data.y[index]]:
-        print ("right")
-    else:
-        print ("false")
+    prediction=completion.choices[0].message.content
+    true_label=data.label_name[data.y[index]]
+
+    with open(write_file, mode='a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([index, prediction, true_label])
+        print([index, prediction, true_label])
 
 
-def arxiv_response(model_name, index):
+def arxiv_response(model_name, index, write_file):
     device = 'cpu'
     data = torch.load(f"../datasets/arxiv.pt").to(device)
     discription = data.raw_texts[index]
-    question = (
-        "Question: Which of the following "
-        "arXiv CS sub-categories does this dataset belong to? Here are the 40 categories: "
-        "'arxiv cs na', 'arxiv cs mm', 'arxiv cs lo', 'arxiv cs cy', 'arxiv cs cr', "
-        "'arxiv cs dc', 'arxiv cs hc', 'arxiv cs ce', 'arxiv cs ni', 'arxiv cs cc', "
-        "'arxiv cs ai', 'arxiv cs ma', 'arxiv cs gl', 'arxiv cs ne', 'arxiv cs sc', "
-        "'arxiv cs ar', 'arxiv cs cv', 'arxiv cs gr', 'arxiv cs et', 'arxiv cs sy', "
-        "'arxiv cs cg', 'arxiv cs oh', 'arxiv cs pl', 'arxiv cs se', 'arxiv cs lg', "
-        "'arxiv cs sd', 'arxiv cs si', 'arxiv cs ro', 'arxiv cs it', 'arxiv cs pf', "
-        "'arxiv cs cl', 'arxiv cs ir', 'arxiv cs ms', 'arxiv cs fl', 'arxiv cs ds', "
-        "'arxiv cs os', 'arxiv cs gt', 'arxiv cs db', 'arxiv cs dl', 'arxiv cs dm'. "
-        "Reply only one category that you think this paper might belong to. Only reply the category name I given without any other words."
-        "Answer:"
-    )
+    question = DEFAULT_QUESTION["arxiv_q"]
 
     client = OpenAI(
         api_key=os.getenv("DASHSCOPE_API_KEY"), # 如果您没有配置环境变量，请在此处用您的API Key进行替换
@@ -168,25 +215,20 @@ def arxiv_response(model_name, index):
             }
         ]
     )
-    print(completion.choices[0].message.content)
 
-    if completion.choices[0].message.content == data.label_name[data.y[index]]:
-        print ("right")
-    else:
-        print ("false")
+    prediction=completion.choices[0].message.content
+    true_label=data.label_name[data.y[index]]
 
-def instagram_response(model_name, index):
+    with open(write_file, mode='a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([index, prediction, true_label])
+        print([index, prediction, true_label])
+
+def instagram_response(model_name, index, write_file):
     device = 'cpu'
     data = torch.load(f"../datasets/instagram.pt").to(device)
     discription = data.raw_texts[index]
-    question = (
-        "Question: Which of the following "
-        "categories does this instagram user belong to? Here are the 2 categories: "
-        "Normal Users, Commercial Users. "
-        "Reply only one category that you think this paper might belong to. "
-        "Only reply the category name I give of the category: Normal Users, Commercial Users, without any other words."
-        "Answer:"
-    )
+    question = DEFAULT_QUESTION["instagram_q"]
 
     client = OpenAI(
         api_key=os.getenv("DASHSCOPE_API_KEY"), # 如果您没有配置环境变量，请在此处用您的API Key进行替换
@@ -201,27 +243,20 @@ def instagram_response(model_name, index):
             }
         ]
     )
-    print(completion.choices[0].message.content)
+    prediction=completion.choices[0].message.content
+    true_label=data.label_name[data.y[index]]
 
-    if completion.choices[0].message.content == data.label_name[data.y[index]]:
-        print ("right")
-    else:
-        print ("false")
+    with open(write_file, mode='a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([index, prediction, true_label])
+        print([index, prediction, true_label])
 
 
-def reddit_response(model_name, index):
+def reddit_response(model_name, index, write_file):
     device = 'cpu'
     data = torch.load(f"../datasets/reddit.pt").to(device)
     discription = data.raw_texts[index]
-    question = (
-        "Question: Which of the following "
-        "categories does this reddit user belong to? Here are the 2 categories: "
-        "Normal Users, Popular Users. "
-        "Popular Users' posted content are often more attractive."
-        "Reply only one category that you think this paper might belong to. "
-        "Only reply the category name I give of the category: Normal Users, Popular Users, without any other words."
-        "Answer:"
-    )
+    question = DEFAULT_QUESTION["reddit_q"]
 
     client = OpenAI(
         api_key=os.getenv("DASHSCOPE_API_KEY"), # 如果您没有配置环境变量，请在此处用您的API Key进行替换
@@ -236,38 +271,92 @@ def reddit_response(model_name, index):
             }
         ]
     )
-    print(completion.choices[0].message.content)
+    prediction=completion.choices[0].message.content
+    true_label=data.label_name[data.y[index]]
 
-    if completion.choices[0].message.content == data.label_name[data.y[index]]:
-        print ("right")
-    else:
-        print ("false")
+    with open(write_file, mode='a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([index, prediction, true_label])
+        print([index, prediction, true_label])
+
+def acc_f1(dataset, write_file):
+    true_labels=[]
+    predict_labels=[]
+    with open(f"../results/LLMPredictor/llm_zero_shot/prediction/{dataset}.csv", 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            true_labels.append(row[2])
+            predict_labels.append(row[1])
+
+    accuracy = accuracy_score(true_labels, predict_labels)
+    print(f'Accuracy: {accuracy:.3f}')
+    f1 = f1_score(true_labels, predict_labels, average='weighted')
+    print(f'F1 Score: {f1:.3f}')
+
+    with open(write_file, mode='a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([f'Accuracy: {accuracy:.3f}',f'F1 Score: {f1:.3f}'])
 
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--dataset", type=str, default="reddit")
+    parser.add_argument("--dataset", type=str, default="arxiv")
     parser.add_argument("--model_name", type=str, default="qwen-turbo")
 
-    index=190
 
     args = parser.parse_args()
-    if args.dataset == "cora":
-        cora_response(args.model_name, index)
-    elif args.dataset == "pubmed":
-        pubmed_response(args.model_name, index)
-    elif args.dataset == "citeseer":
-        citeseer_response(args.model_name, index)
-    elif args.dataset == "wikics":
-        wikics_response(args.model_name, index)
-    elif args.dataset == "arxiv":
-        arxiv_response(args.model_name, index)
-    elif args.dataset == "instagram":
-        instagram_response(args.model_name, index)
-    elif args.dataset == "reddit":
-        reddit_response(args.model_name, index)
-    else:
-        print ("Error, no such dataset.")
-        exit(1)
+
+    device = 'cpu'
+
+    # get the index set of the training set on given dataset
+    data = torch.load(f"../datasets/{args.dataset}.pt").to(device)
+    test_mask = data.test_mask[0] if len(data.test_mask) == 10 else data.test_mask
+    test_indexes = torch.where(test_mask == True)[0].cpu().numpy().tolist()
+
+    test_indexes=[89,37,11,38,666,727]
+
+    # create csv file
+    os.makedirs("../results/LLMPredictor/llm_zero_shot/prediction", exist_ok=True)
+    write_file = open(f"../results/LLMPredictor/llm_zero_shot/prediction/{args.dataset}.csv", 'a',newline='')
+    write_file_path = f"../results/LLMPredictor/llm_zero_shot/prediction/{args.dataset}.csv"
+
+    #test
+    for index in test_indexes:
+
+        #test if already have
+        whether_to_predict=1
+
+        with open (f"../results/LLMPredictor/llm_zero_shot/prediction/{args.dataset}.csv",'r') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                if int(row[0]) == int(index):
+                    whether_to_predict=0
+                    break
+
+        if whether_to_predict == 0:
+            continue
+
+        #prediction
+        if args.dataset == "cora":
+            cora_response(args.model_name, index, write_file_path)
+        elif args.dataset == "pubmed":
+            pubmed_response(args.model_name, index, write_file_path)
+        elif args.dataset == "citeseer":
+            citeseer_response(args.model_name, index, write_file_path)
+        elif args.dataset == "wikics":
+            wikics_response(args.model_name, index, write_file_path)
+        elif args.dataset == "arxiv":
+            arxiv_response(args.model_name, index, write_file_path)
+        elif args.dataset == "instagram":
+            instagram_response(args.model_name, index, write_file_path)
+        elif args.dataset == "reddit":
+            reddit_response(args.model_name, index, write_file_path)
+        else:
+            print("Error, no such dataset.")
+            exit(1)
+
+    # calculate acc and f1
+    acc_f1(args.dataset,write_file_path)
+
