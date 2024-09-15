ad8c4d3506db8047832c34e866874a1f9f8da5b3
Noah0401 <165160435+Noah0401@users.noreply.github.com>
2024-09-15 21:38:26 +0800

    Update zeroshot_llm.py

--- LLMPredictor/zeroshot_llm.py
+++ LLMPredictor/zeroshot_llm.py
@@ -53,8 +53,58 @@ ARXIV_Q = """Question: Which of the following
         'arxiv cs sd', 'arxiv cs si', 'arxiv cs ro', 'arxiv cs it', 'arxiv cs pf', 
         'arxiv cs cl', 'arxiv cs ir', 'arxiv cs ms', 'arxiv cs fl', 'arxiv cs ds', 
         'arxiv cs os', 'arxiv cs gt', 'arxiv cs db', 'arxiv cs dl', 'arxiv cs dm'. 
+        Use the words in this part to answer me, not the explanation part bellow.
+        
+        Here are the explanation of each category:
+        'cs.AI (Artificial Intelligence)',
+
+        'cs.AR (Hardware Architecture)',
+        
+        'cs.CC (Computational Complexity)',
+        
+        'cs.CE (Computational Engineering, Finance, and Science)',
+        'cs.CG (Computational Geometry)',
+        
+        'cs.CL (Computation and Language)',
+        
+        'cs.CR (Cryptography and Security)',
+        'cs.CV (Computer Vision and Pattern Recognition)',
+        'cs.CY (Computers and Society)',
+        'cs.DB (Databases)',
+        'cs.DC (Distributed, Parallel, and Cluster Computing)',
+        'cs.DL (Digital Libraries)',
+        'cs.DM (Discrete Mathematics)',
+        'cs.DS (Data Structures and Algorithms)',
+        'cs.ET (Emerging Technologies)',
+        'cs.FL (Formal Languages and Automata Theory)',
+        'cs.GL (General Literature)',
+        'cs.GR (Graphics)',
+        'cs.GT (Computer Science and Game Theory)',
+        'cs.HC (Human-Computer Interaction)',
+        
+        'cs.IR (Information Retrieval)',
+        'cs.IT (Information Theory)',
+        'cs.LG (Machine Learning)',
+        'cs.LO (Logic in Computer Science)',
+        'cs.MA (Multiagent Systems)',
+        'cs.MM (Multimedia)',
+        'cs.MS (Mathematical Software)',
+        'cs.NA (Numerical Analysis)',
+        'cs.NE (Neural and Evolutionary Computing)',
+        'cs.NI (Networking and Internet Architecture)',
+        'cs.OH (Other Computer Science)',
+        'cs.OS (Operating Systems)',
+        'cs.PF (Performance)',
+        'cs.PL (Programming Languages)',
+        'cs.RO (Robotics)',
+        'cs.SC (Symbolic Computation)',
+        'cs.SD (Sound)',
+        'cs.SE (Software Engineering)',
+        'cs.SI (Social and Information Networks)',
+        'cs.SY (Systems and Control)'
         Reply only one category that you think this paper might belong to. 
-        Only reply the category name I given without any other words, please don't use your own words.
+        Only reply the category name (not the explanation) I given without any other words, please don't use your own words.
+        Be careful, only use the name of the category I give you, not the explanation part or any other words.
         Answer:
     """
 
@@ -85,17 +135,19 @@ DEFAULT_QUESTION = {
     "reddit_q": REDDIT_Q,
 }
 
-def cora_response(model_name, index, write_file):
+def get_response(dataset, model_name, index, write_file_path):
     device = 'cpu'
-    data = torch.load(f"../datasets/cora.pt").to(device)
+    data = torch.load(f"../datasets/{dataset}.pt").to(device)
 
     discription = data.raw_texts[index]
-    question = DEFAULT_QUESTION["cora_q"]
+    question = DEFAULT_QUESTION[f"{dataset}_q"]
 
     client = OpenAI(
-        api_key=os.getenv("DASHSCOPE_API_KEY"), # 如果您没有配置环境变量，请在此处用您的API Key进行替换
+        #api_key=os.getenv("DASHSCOPE_API_KEY"), # 如果您没有配置环境变量，请在此处用您的API Key进行替换
+        api_key="sk-6ed3b105aaac459097168fd8cca58513",
         base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",  # 填写DashScope服务的base_url
     )
+
     completion = client.chat.completions.create(
         model=model_name,
         messages=[
@@ -109,193 +161,32 @@ def cora_response(model_name, index, write_file):
     prediction=completion.choices[0].message.content
     true_label=data.label_name[data.y[index]]
 
-    with open(write_file, mode='a', newline='') as file:
+    with open(write_file_path, mode='a', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow([index, prediction, true_label])
-        print([index, prediction, true_label])
+        writer.writerow([model_name, index, prediction, true_label])
+        print([model_name, index, prediction, true_label])
 
-def pubmed_response(model_name, index, write_file):
-    device = 'cpu'
-    data = torch.load(f"../datasets/pubmed.pt").to(device)
-    discription = data.raw_texts[index]
-    question = DEFAULT_QUESTION["pubmed_q"]
-
-    client = OpenAI(
-        api_key=os.getenv("DASHSCOPE_API_KEY"), # 如果您没有配置环境变量，请在此处用您的API Key进行替换
-        base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",  # 填写DashScope服务的base_url
-    )
-    completion = client.chat.completions.create(
-        model=model_name,
-        messages=[
-            {
-                'role': 'user',
-                'content': f"{discription}\n{question}"
-            }
-        ]
-    )
-    prediction=completion.choices[0].message.content
-    true_label=data.label_name[data.y[index]]
-
-    with open(write_file, mode='a', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow([index, prediction, true_label])
-        print([index, prediction, true_label])
-
-def citeseer_response(model_name, index, write_file):
-    device = 'cpu'
-    data = torch.load(f"../datasets/citeseer.pt").to(device)
-    discription = data.raw_texts[index]
-    question = DEFAULT_QUESTION["citeseer_q"]
-
-    client = OpenAI(
-        api_key=os.getenv("DASHSCOPE_API_KEY"), # 如果您没有配置环境变量，请在此处用您的API Key进行替换
-        base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",  # 填写DashScope服务的base_url
-    )
-    completion = client.chat.completions.create(
-        model=model_name,
-        messages=[
-            {
-                'role': 'user',
-                'content': f"{discription}\n{question}"
-            }
-        ]
-    )
-    prediction=completion.choices[0].message.content
-    true_label=data.label_name[data.y[index]]
-
-    with open(write_file, mode='a', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow([index, prediction, true_label])
-        print([index, prediction, true_label])
-
-def wikics_response(model_name, index, write_file):
-    device = 'cpu'
-    data = torch.load(f"../datasets/wikics.pt").to(device)
-    discription = data.raw_texts[index]
-    question = DEFAULT_QUESTION["wikics_q"]
-
-    client = OpenAI(
-        api_key=os.getenv("DASHSCOPE_API_KEY"), # 如果您没有配置环境变量，请在此处用您的API Key进行替换
-        base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",  # 填写DashScope服务的base_url
-    )
-    completion = client.chat.completions.create(
-        model=model_name,
-        messages=[
-            {
-                'role': 'user',
-                'content': f"{discription}\n{question}"
-            }
-        ]
-    )
-    prediction=completion.choices[0].message.content
-    true_label=data.label_name[data.y[index]]
-
-    with open(write_file, mode='a', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow([index, prediction, true_label])
-        print([index, prediction, true_label])
-
-
-def arxiv_response(model_name, index, write_file):
-    device = 'cpu'
-    data = torch.load(f"../datasets/arxiv.pt").to(device)
-    discription = data.raw_texts[index]
-    question = DEFAULT_QUESTION["arxiv_q"]
-
-    client = OpenAI(
-        api_key=os.getenv("DASHSCOPE_API_KEY"), # 如果您没有配置环境变量，请在此处用您的API Key进行替换
-        base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",  # 填写DashScope服务的base_url
-    )
-    completion = client.chat.completions.create(
-        model=model_name,
-        messages=[
-            {
-                'role': 'user',
-                'content': f"{discription}\n{question}"
-            }
-        ]
-    )
-
-    prediction=completion.choices[0].message.content
-    true_label=data.label_name[data.y[index]]
-
-    with open(write_file, mode='a', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow([index, prediction, true_label])
-        print([index, prediction, true_label])
-
-def instagram_response(model_name, index, write_file):
-    device = 'cpu'
-    data = torch.load(f"../datasets/instagram.pt").to(device)
-    discription = data.raw_texts[index]
-    question = DEFAULT_QUESTION["instagram_q"]
-
-    client = OpenAI(
-        api_key=os.getenv("DASHSCOPE_API_KEY"), # 如果您没有配置环境变量，请在此处用您的API Key进行替换
-        base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",  # 填写DashScope服务的base_url
-    )
-    completion = client.chat.completions.create(
-        model=model_name,
-        messages=[
-            {
-                'role': 'user',
-                'content': f"{discription}\n{question}"
-            }
-        ]
-    )
-    prediction=completion.choices[0].message.content
-    true_label=data.label_name[data.y[index]]
-
-    with open(write_file, mode='a', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow([index, prediction, true_label])
-        print([index, prediction, true_label])
-
-
-def reddit_response(model_name, index, write_file):
-    device = 'cpu'
-    data = torch.load(f"../datasets/reddit.pt").to(device)
-    discription = data.raw_texts[index]
-    question = DEFAULT_QUESTION["reddit_q"]
-
-    client = OpenAI(
-        api_key=os.getenv("DASHSCOPE_API_KEY"), # 如果您没有配置环境变量，请在此处用您的API Key进行替换
-        base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",  # 填写DashScope服务的base_url
-    )
-    completion = client.chat.completions.create(
-        model=model_name,
-        messages=[
-            {
-                'role': 'user',
-                'content': f"{discription}\n{question}"
-            }
-        ]
-    )
-    prediction=completion.choices[0].message.content
-    true_label=data.label_name[data.y[index]]
-
-    with open(write_file, mode='a', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow([index, prediction, true_label])
-        print([index, prediction, true_label])
-
-def acc_f1(dataset, write_file):
+def acc_f1(dataset, file_path, model_name):
     true_labels=[]
     predict_labels=[]
-    with open(f"../results/LLMPredictor/llm_zero_shot/prediction/{dataset}.csv", 'r') as file:
+    with open(file_path, 'r') as file:
         reader = csv.reader(file)
         for row in reader:
-            true_labels.append(row[2])
-            predict_labels.append(row[1])
+            if str(row[0]) == model_name:
+                if str(str(row[1])[0]) == "A":
+                    continue
+                true_labels.append(row[3])
+                predict_labels.append(row[2])
+
 
     accuracy = accuracy_score(true_labels, predict_labels)
     print(f'Accuracy: {accuracy:.3f}')
     f1 = f1_score(true_labels, predict_labels, average='weighted')
     print(f'F1 Score: {f1:.3f}')
 
-    with open(write_file, mode='a', newline='') as file:
+    with open(file_path, mode='a', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow([f'Accuracy: {accuracy:.3f}',f'F1 Score: {f1:.3f}'])
+        writer.writerow([model_name, f'Accuracy: {accuracy:.3f}',f'F1 Score: {f1:.3f}'])
 
 
 
@@ -315,21 +206,23 @@ if __name__ == '__main__':
     test_mask = data.test_mask[0] if len(data.test_mask) == 10 else data.test_mask
     test_indexes = torch.where(test_mask == True)[0].cpu().numpy().tolist()
 
-    # create csv file
-    os.makedirs("../results/LLMPredictor/llm_zero_shot/prediction", exist_ok=True)
-    write_file = open(f"../results/LLMPredictor/llm_zero_shot/prediction/{args.dataset}.csv", 'a',newline='')
-    write_file_path = f"../results/LLMPredictor/llm_zero_shot/prediction/{args.dataset}.csv"
+    test_indexes=[1,2,3,4,5]
 
-    #test
+    # create csv file
+    os.makedirs("../results/LLMPredictor/llm_zero_shot", exist_ok=True)
+    file_path = f"../results/LLMPredictor/llm_zero_shot/{args.dataset}.csv"
+    write_file = open(file_path, 'a', newline='')
+
+    #make predictions
     for index in test_indexes:
 
         #test if already have
         whether_to_predict=1
 
-        with open (f"../results/LLMPredictor/llm_zero_shot/prediction/{args.dataset}.csv",'r') as file:
+        with open (file_path,'r') as file:
             reader = csv.reader(file)
             for row in reader:
-                if int(row[0]) == int(index):
+                if str(row[0]) == str(args.model_name) and str(row[1]) == str(index):
                     whether_to_predict=0
                     break
 
@@ -337,24 +230,8 @@ if __name__ == '__main__':
             continue
 
         #prediction
-        if args.dataset == "cora":
-            cora_response(args.model_name, index, write_file_path)
-        elif args.dataset == "pubmed":
-            pubmed_response(args.model_name, index, write_file_path)
-        elif args.dataset == "citeseer":
-            citeseer_response(args.model_name, index, write_file_path)
-        elif args.dataset == "wikics":
-            wikics_response(args.model_name, index, write_file_path)
-        elif args.dataset == "arxiv":
-            arxiv_response(args.model_name, index, write_file_path)
-        elif args.dataset == "instagram":
-            instagram_response(args.model_name, index, write_file_path)
-        elif args.dataset == "reddit":
-            reddit_response(args.model_name, index, write_file_path)
-        else:
-            print("Error, no such dataset.")
-            exit(1)
+        get_response(args.dataset, args.model_name, index, file_path)
 
     # calculate acc and f1
-    acc_f1(args.dataset,write_file_path)
+    acc_f1(args.dataset, file_path, args.model_name)
 
