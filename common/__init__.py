fb0795ee8a8e77a1f56ac13a6e61bd1c535bc6d1
WxxShirley <WxxShirley@outlook.com>
2024-09-16 23:02:52 +0800

    add TAPE (init)

--- common/__init__.py
+++ common/__init__.py
@@ -1,5 +1,6 @@
-from .dataloader import load_graph_dataset
+from .dataloader import load_graph_dataset, load_graph_dataset_for_tape
 from .gnn import GNNEncoder
 from .prompt import ZEROSHOT_PROMPTS
 from .utils import set_seed, get_cur_time, array_mean_std
 from .metrics import *
+from .lm import BertClassifier, BertClaInfModel
--- common/dataloader.py
+++ common/dataloader.py
@@ -14,3 +14,14 @@ def load_graph_dataset(dataset_name, device, emb_model="shallow"):
         graph_data.train_mask, graph_data.val_mask, graph_data.test_mask = graph_data.train_mask[0], graph_data.val_mask[0], graph_data.test_mask[0]
     
     return graph_data
+
+
+def load_graph_dataset_for_tape(dataset_name, device, use_gpt=False):
+    graph_data = torch.load(f"../../datasets/{dataset_name}.pt").to(device)
+    
+    if len(graph_data.train_mask) == 10:
+        graph_data.train_mask, graph_data.val_mask, graph_data.test_mask = graph_data.train_mask[0], graph_data.val_mask[0], graph_data.test_mask[0]
+    
+    # TODO: fill use_gpt
+
+    return graph_data, len(graph_data.label_name), graph_data.raw_texts
