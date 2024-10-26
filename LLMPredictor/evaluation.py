1c501a9b3a6d92682e7b975f0fbe60f1b325cb29
Noah0401 <165160435+Noah0401@users.noreply.github.com>
2024-10-26 21:26:45 +0800

    Update evaluation.py

--- LLMPredictor/evaluation.py
+++ LLMPredictor/evaluation.py
@@ -47,6 +47,11 @@ def check_correct(dataset, row):
         new_labels = [item.replace('_', ' ') for item in false_labels]
         false_labels.extend(new_labels)
 
+        # '_' ->'-'
+        true_labels.append(row[2].replace('_', '-'))
+        new_labels = [item.replace('_', '-') for item in false_labels]
+        false_labels.extend(new_labels)
+
         # lower case
         true_labels.append(row[2].lower())
         true_labels.append(true_labels[1].lower())
@@ -74,8 +79,14 @@ def check_correct(dataset, row):
         new_labels = [item[:-1] for item in false_labels]
         false_labels.extend(new_labels)
 
+        # no "Users"
+        true_labels.append(row[2][:-6])
+        new_labels = [item[:-6] for item in false_labels]
+        false_labels.extend(new_labels)
+
     true_labels_in_completion = [x in row[1] for x in true_labels]
     false_label_in_completion = [x in row[1] for x in false_labels]
+
 
     if any(true_labels_in_completion) and not any(false_label_in_completion):
         return "correct"
@@ -126,41 +137,47 @@ def evaluate(file_path, model_name, dataset):
             if row[0][0]< '0' or row[0][0] > '9':
                 continue
             
+
             total_num += 1
-            print(row)
             if row[3] == "correct":
                 true_labels.append(row[2])
                 predict_labels.append(row[2])
 
             elif row[3] == "wrong":
-                i=0
                 found = 0
                 true_labels.append(row[2])
                 for item in labels[dataset]:
 
                     if dataset == "cora":
-                        if item.replace('_', ' ') in row[1] or item.lower() in row[1] or item in row[1]:
-                            predict_labels.append(item)
-
+                        if item.replace('_', ' ') in row[1] or item.lower() in row[1] or item in row[1] or item.replace('_', '-') in row[1]: 
+                            found += 1
+                            if found <= 1:
+                                predict_labels.append(item)
+        
                     elif dataset == "citeseer":
-                        i += 1
                         
                         if item == "Agents":
                             if item in row[1]:
-                                predict_labels.append(item)
-                                found = 1
+                                found += 1
+                                if found <= 1:
+                                    predict_labels.append(item)
                         else:
                             if item.split('(')[1].replace(')','') in row[1] or item.split(' (')[0] in row[1] or item in row[1]:
-                                predict_labels.append(item)
-                                found = 1
+                                found += 1
+                                if found <= 1:
+                                    predict_labels.append(item)
                     
                     elif dataset == "reddit" or dataset == "instagram":
-                        if item[:-1] in row[1] or item in row[1]:
-                            predict_labels.append(item)
+                        if item[:-1] in row[1] or item in row[1] or item[:-6] in row[1]:
+                            found += 1
+                            if found <= 1:
+                                predict_labels.append(item)
                     
                     else:
                         if item in row[1]:
-                            predict_labels.append(item)
+                            found += 1
+                            if found <= 1:
+                                predict_labels.append(item)
             
             elif row[3] == "uncertain":
                 predict_labels.append(row[1])
