526a1bc698fa28c6d73417abdf6afdb224b3b299
Noah0401 <gfz20040401@163.com>
2024-12-23 21:32:03 +0800

    run

--- LLMZeroshot/run.sh
+++ LLMZeroshot/run.sh
@@ -4,6 +4,8 @@ for DATASET in  photo cora pubmed wikics instagram ; do
             python zeroshot_llm_main.py --prediction_type=$PREDICTION_TYPE --model_name=$MODELNAME --dataset=$DATASET
     done
 done
+
+
 # source /data/fge2/try/.venv/bin/activate.csh
 # nohup python3 -u zeroshot_llm_main.py >>../results/LLMPredictor/llm_zero_shot/gpt-4/cora.log
 # nohup ./run.sh >/data/fge2/try/LLMGraphBench/results/zero_shot/gpt4o-run.log
